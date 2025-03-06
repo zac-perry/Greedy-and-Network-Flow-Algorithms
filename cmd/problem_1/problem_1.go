032ca64d1ac7cafffba78c47d5de04f9cc61e111
zperry4 <zactperry73@gmail.com>
2025-03-05 20:40:08 -0500

    updates, freq table

--- cmd/problem_1/problem_1.go
+++ cmd/problem_1/problem_1.go
@@ -1,9 +1,86 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"slices"
 )
 
+// TODO: All of the huffman encoding shit
+/*
+Greedy Algorithms: Write an algorithm in the language of your choice to provide
+a Huffman encoding from the contents of a .txt file (just the 26 lower case letters
+of the alphabet in some arbitrary combination and length) and apply it to
+compress the file. Your program should output 2 things, the frequency table for
+the characters, and the size of the file (in bytes) before and after the encoding.
+Note: You will build the frequency table based on the content of the file.
+*/
+
+// General Approach
+/*
+1. Read in the file and count frequency of each char
+2. Then, build a Huffman tree for each char based on their frequency (look up algo for this)
+  - NOTE: this will be the hard part
+3. Then, traverse this tree, creating the encoding for each letter
+4. Store the encoding, (letter as the key, encoded value as the val
+5. When printing, traverse the original file again, and look up each letter to get the encoding
+*/
+
+// printFreqTable will just print out the chars and their corresponding frequency of
+// occurence within the file
+func printFreqTable(charFreqMap map[byte]int) {
+	chars := make([]byte, 0)
+	for c := range charFreqMap {
+		chars = append(chars, c)
+	}
+	slices.Sort(chars)
+
+	fmt.Println("Length of freq: ", len(charFreqMap))
+	fmt.Println("CHAR | FREQ")
+	fmt.Println("============")
+
+	for _, c := range chars {
+		fmt.Printf("%-4s | %4d\n", string(c), charFreqMap[c])
+	}
+}
+
+// readFile just takes the corresponding file name and opens it
+// It will then read in all chars within the file (lowercase)
+// Each char will be stored in a map with the corresponding frequency of occurence
+func readFile(fileName string) map[byte]int {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal("Error opening the file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanRunes)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	charFreqMap := make(map[byte]int)
+	for scanner.Scan() {
+		char := byte(scanner.Text()[0])
+
+		if char >= 97 && char <= 122 {
+			charFreqMap[char]++
+		}
+	}
+
+	return charFreqMap
+}
+
 func main() {
-	fmt.Println("wow")
+	if len(os.Args) < 2 {
+		log.Fatal("usage: ./bin/problem_1 filename")
+	}
+
+	freq := readFile(os.Args[1])
+	printFreqTable(freq)
 }
--- cmd/problem_2/problem_2.go
+++ cmd/problem_2/problem_2.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+// TODO: network flow
+/*
+2. Network Flow: Write an algorithm in a language of your choice that takes as input
+a flow network in modified DIMACS format and prints all flow augmenting paths.
+Ensure infinite looping isn’t possible. I will try to break your code.
+Ex. File (tab separated values)
+
+5 6
+0 1 4
+0 2 3
+0 3 5
+1 4 7
+2 4 18
+3 4 6
+
+The first row is the vertex count and then the arc count
+The remaining rows are the arcs: vertex 1, vertex 2, and arc weight
+0 will always be the source vertex, and the vertex with the largest id will always
+be the sink. In the above example that makes 0 the source and 4 the sink.
+*/
 func main() {
 	fmt.Println("wow2")
 }
